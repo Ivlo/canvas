6b7b0347311bb1f06f95362f1c76a459123f70cf
Iván Lòpez <ivlo@MacBook-Pro-de-Ivan-3.local>
2013-11-04 10:05:02 +0100

    rotacion

---
+++ utils.js
@@ -0,0 +1,12 @@
+//Normalize use requestAnimationFrame
+if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = (window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame || function(callback) {
+        return window.setTimeout(callback, 17 /*~ 1000/60*/ );
+    });
+}
+
+//Normalize use cancelAnimationFrame
+
+if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = (window.cancelRequestAnimationFrame || window.webkitCancelAnimationFrame || window.webkitCancelRequestAnimationFrame || window.mozCancelAnimationFrame || window.mozCancelRequestAnimationFrame || window.msCancelAnimationFrame || window.msCancelRequestAnimationFrame || window.oCancelAnimationFrame || window.oCancelRequestAnimationFrame || window.clearTimeout);
+}
